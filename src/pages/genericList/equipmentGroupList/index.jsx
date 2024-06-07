50e2614a73f64198db8892d54121f343d8e047a9
PenVsse <vusiphu20042000@gmail.com>
2024-06-07 11:38:03 +0700

    up

--- src/pages/genericList/equipmentGroupList/index.jsx
+++ src/pages/genericList/equipmentGroupList/index.jsx
@@ -1,9 +1,13 @@
 import { getAllEquipType } from "@/apis/equip-type";
 import { AgGrid } from "@/components/aggridreact/AgGrid";
 import { DateTimeRenderByText } from "@/components/aggridreact/cellRender";
+import { BtnAddRow } from "@/components/aggridreact/tableTools/BtnAddRow";
+import { BtnSave } from "@/components/aggridreact/tableTools/BtnSave";
+import { GrantPermission } from "@/components/common";
 import { useCustomToast } from "@/components/custom-toast";
 import { SearchInput } from "@/components/search";
 import { Section } from "@/components/section";
+import { actionGrantPermission } from "@/constants";
 import React, { useRef, useState } from "react";
 
 export function EquipmentGroupList() {
@@ -48,11 +52,24 @@ export function EquipmentGroupList() {
       <Section>
         <Section.Header title="Danh mục loại thiết bị"></Section.Header>
         <Section.Content>
-          <SearchInput
-            handleSearch={value => {
-              setSearchData(value);
-            }}
-          />
+          <span className="flex justify-between">
+            <SearchInput
+              handleSearch={value => {
+                setSearchData(value);
+              }}
+            />
+            <span>
+              <div className="mb-2 text-xs font-medium">Công cụ</div>
+              <div className="flex h-[42px] items-center gap-x-3 rounded-md bg-gray-100 px-6">
+                <GrantPermission action={actionGrantPermission.CREATE}>
+                  <BtnAddRow onAddRow={() => {}} />
+                </GrantPermission>
+                <GrantPermission action={actionGrantPermission.UPDATE}>
+                  <BtnSave onClick={() => {}} />
+                </GrantPermission>
+              </div>
+            </span>
+          </span>
           <AgGrid
             contextMenu={true}
             setRowData={data => {
