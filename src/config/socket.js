732ebaa59e44393e436228e60e807acbffcc53e4
thiennhi <thienphamnhi@gmail.com>
2024-07-14 11:03:13 +0700

    fix env

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3050";
+const URL = import.meta.env.VITE_BASE_URL || "http://localhost:3050";
 
 export const socket = io(URL, {
   autoConnect: false
