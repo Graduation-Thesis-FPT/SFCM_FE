3cc571307ff091535716890ca2d872b75ac4d20c
thiennhi <thienphamnhi@gmail.com>
2024-07-18 01:44:05 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import Router from "./routes/Router";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import { Toaster } from "@/components/common/ui/toaster";
-import { AuthProvider } from "./routes/AuthProvider";
-import { registerLicense } from "@syncfusion/ej2-base";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { Toaster } from './components/common/ui/toaster';
+import { AuthProvider } from './routes/AuthProvider';
+import Router from './routes/Router';
+import { registerLicense } from '@syncfusion/ej2-base';
+import './index.scss';
 
 registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <AuthProvider> */}
-          {/* <Router /> */}
-        {/* </AuthProvider> */}
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+        <Toaster />
       </BrowserRouter>
-      <Toaster />
     </Provider>
   </React.StrictMode>
 );
