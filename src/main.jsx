e5dfb5fd351b2a6d224d1d0694e6806423013d42
thiennhi <thienphamnhi@gmail.com>
2024-07-18 01:35:47 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -15,9 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <AuthProvider>
+        {/* <AuthProvider> */}
           {/* <Router /> */}
-        </AuthProvider>
+        {/* </AuthProvider> */}
       </BrowserRouter>
       <Toaster />
     </Provider>
