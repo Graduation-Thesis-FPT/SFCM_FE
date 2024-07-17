7339b23046bc1f0b7e530028320f3e75c6069875
thiennhi <thienphamnhi@gmail.com>
2024-07-18 00:08:18 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import Router from "@/routes/Router";
 import { store } from "./redux/store";
 import { Toaster } from "@/components/common/ui/toaster";
 import { AuthProvider } from "./routes/AuthProvider";
+import { Router } from "./routes/Router";
 // import { registerLicense } from "@syncfusion/ej2-base";
 
 // registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
