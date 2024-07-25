b90e1530eca8694cd6b59ecb11f7e6acb60b1c92
PenVsse <vusiphu20042000@gmail.com>
2024-07-26 00:39:32 +0700

    fix dc cai bug xam xi cua date picker roi vui qua di ca nha oi

--- src/components/common/date-picker/index.jsx
+++ src/components/common/date-picker/index.jsx
@@ -8,32 +8,30 @@ import { vi } from "date-fns/locale";
 
 export function DatePicker({ className, onSelected, date }) {
   return (
-    <div>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "w-full justify-start text-left font-normal",
-              !date && "text-muted-foreground",
-              className
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date ? format(date, "LLL dd, y", { locale: vi }) : <span>Chọn ngày</span>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="single"
-            selected={date}
-            onSelect={onSelected}
-            numberOfMonths={1}
-          />
-        </PopoverContent>
-      </Popover>
-    </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          id="date"
+          variant={"outline"}
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+            className
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "LLL dd, y", { locale: vi }) : <span>Chọn ngày</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="pointer-events-auto w-auto p-0" align="start">
+        <Calendar
+          initialFocus
+          mode="single"
+          selected={date}
+          onSelect={onSelected}
+          numberOfMonths={1}
+        />
+      </PopoverContent>
+    </Popover>
   );
 }
