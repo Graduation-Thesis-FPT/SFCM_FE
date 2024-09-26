103a6d231964b49bb5895a2c2396f9620bc7cc10
thiennhi <pthiennhi.it@gmail.com>
2024-09-26 14:24:41 +0700

    fix: revert adding :443 to socket.io url

--- src/config/socket.js
+++ src/config/socket.js
@@ -2,10 +2,6 @@ import { io } from "socket.io-client";
 
 let URL = import.meta.env.VITE_BASE_URL || "http://localhost:3050";
 
-if (URL.startsWith("https")) {
-  URL += ":443";
-}
-
 export const socket = io(URL, {
   autoConnect: false
 });
