ec299c8320669bbcb876546c859fb95a46d98ee3
Minh Vu <minhngoc02hn@gmail.com>
2024-09-26 13:45:23 +0700

    fix: add :443 to socket io client url

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_BASE_URL || "http://localhost:3050";
+let URL = import.meta.env.VITE_BASE_URL || "http://localhost:3050";
+
+if (URL.startsWith("https")) {
+  URL += ":443";
+}
 
 export const socket = io(URL, {
   autoConnect: false
